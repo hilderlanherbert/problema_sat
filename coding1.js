2474cb137563bbe22d38ed2fbe820f7e2d59d592
hilderlanherbert <hilderlan.herbert@gmail.com>
2017-11-23 17:01:52 -0300

    second commit

--- coding1.js
+++ coding1.js
@@ -1,11 +1,26 @@
 fs = require('fs')
+var variablesP;
+var clausesP;
 
 function readFormula(fileName) {
-  var file = fs.readFileSync("sat/" + fileName, 'ascii')
-  var text = file.split("\n")
+  var file = fs.readFileSync("sat/" + fileName, 'ascii');
+  var text = file.split("\n");
 
-  for (var i=0; i < file.lenght(); i++){
-    var variables [i] =
+  for (ss in text){
+    if(ss[0] != 'c'){
+      for(var j=0; j < ss.lenght; j++ ){
+        if(ss[0] == "p"){
+          //pegar paramentros v e c
+           var tt = ss.split(" ");
+            variablesP = ParseInt(tt[tt.lenght -2]);
+            clausesP = ParseInt(tt[tt.lenght -1]);
+        }else[
+          //
+        numbers.push(ss);
+        ]
+      }
+    }
+     [i] =
     var clauses [i] =
   }
 
