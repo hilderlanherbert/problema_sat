c478a7be8b03c52e152a5e70e7224a792649e1ae
hilderlanherbert <hilderlan.herbert@gmail.com>
2017-12-04 22:20:14 -0300

    Finish readFormula and checkProblemSpecification

--- coding1.js
+++ coding1.js
@@ -1,29 +1,65 @@
-fs = require('fs')
+fs = require('fs');
+
 var variablesP;
 var clausesP;
+var variables;
+var clauses = [];
+var clause = [];
 
-function readFormula(fileName) {
-  var file = fs.readFileSync("sat/" + fileName, 'ascii');
-  var text = file.split("\n");
+function checkProblemSpecification(){
 
-  for (ss in text){
-    if(ss[0] != 'c'){
-      for(var j=0; j < ss.lenght; j++ ){
-        if(ss[0] == "p"){
-          //pegar paramentros v e c
-           var tt = ss.split(" ");
-            variablesP = ParseInt(tt[tt.lenght -2]);
-            clausesP = ParseInt(tt[tt.lenght -1]);
-        }else[
-          //
-        numbers.push(ss);
-        ]
+
+  var merged = [].concat.apply([], clauses).map(Number).map(Math.abs);
+
+  var unique = merged.filter(function(elem, index, self) {
+      return index === self.indexOf(elem);
+  });
+
+  return (variablesP === unique.length && clausesP === clauses.length);
+}
+
+function readFormula(filename) {
+
+  var file = fs.readFileSync("sat/" + filename, 'ascii');
+  var cnf_rows = file.split("\n");
+
+  for (row of cnf_rows){
+    if(row[0] != 'c'){
+      if(row[0] == "p"){
+        //pegar paramentros v e c
+          var problem;
+          problem = row.split(" ");
+          variablesP = parseInt(problem[problem.length-2]);
+          variables = new Array(variablesP);
+          variables = variables.fill(0);
+          clausesP = parseInt(problem[problem.length-1]);
+      }else{
+        //pegar numeros
+        clause = clause.concat(row.split(" "));
+ 
+        if(clause[clause.length - 1] == "0"){
+          clause = clause.slice(0, clause.length - 1);
+          clauses.push(clause);
+          clause = [];
+        }
       }
     }
-     [i] =
-    var clauses [i] =
+  }
+
+  if(checkProblemSpecification()){;
+
+    var object = {
+      "clauses": clauses,
+      "variables": variables
+    }
+
+    return object;
+
+  }else{
+
+    return "Erro: Conjunto de cláusulas difere da definição do problema."
   }
 
 }
 
-readFormula('hole1.cnf')
+console.log(readFormula('hole1.cnf'));
