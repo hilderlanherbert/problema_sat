b317dce766cd913645242b40f6196d782f35dc96
hilderlanherbert <hilderlan.herbert@gmail.com>
2017-12-07 00:33:37 -0300

    Bugfix and add comments.

--- coding1.js
+++ coding1.js
@@ -72,19 +72,20 @@ function readFormula(filename) {
 function nextAssignment(variables) {
 
   if(fixe < variables.length){
-    
+    //colocar 1 a esquerda do fixe
     for(var i=0; i < fixe; i++){
       variables[i] = 1;
     }
-
+    // alterna o 1 a partir de uma posição a direita do fixe
     for(var j=fixe; j < variables.length; j++){
-      if((j!= currentPosition) || (j == currentPosition && currentPosition == fixe)){
-        variables[j] = 0;
-      }else{
+      if(j == currentPosition && j != fixe){
         variables[j] = 1;
+      }else{
+        variables[j] = 0;
       }
     }
 
+    //quando o currentPosition chega na posição mais a direita o fixe é incrementado senão apenas o currentPosition é incrementado
     if(currentPosition == variables.length - 1){
       ++fixe;
       currentPosition = fixe;
@@ -96,6 +97,8 @@ function nextAssignment(variables) {
   
   }else if(fixe == variables.length){
 
+    //quando fixe chega na última posição mais a direita(fixe == tamanho do vetor) retorna um vetor com todas as posições igual a 1
+
     for(var i=0; i < variables.length; i++){
       variables[i] = 1;
     }
@@ -104,6 +107,9 @@ function nextAssignment(variables) {
     return variables;
 
   }else{
+
+    // quando fixe > tamanho do vetor significa que todas as possibilidades foram testadas e retorna -1.
+
     return -1;
   }
 }
@@ -156,4 +162,4 @@ function solve(file){
   return doSolve(expression.clauses, expression.variables);
 }
 
-console.log(solve('hole6.cnf'));
+console.log(solve('hole1.cnf'));
